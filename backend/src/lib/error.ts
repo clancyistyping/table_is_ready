2f7d5b823b2d6d53cff697de5e37eb83c02d13e2
clancyistyping <mykim1891@gmail.com>
2026-02-26 01:51:12 -0800

    Fixing dotenv problem in CI environment

--- backend/src/lib/error.ts
+++ backend/src/lib/error.ts
@@ -1,20 +1,20 @@
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
-import { FastifyReply } from "fastify";
+// import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
+// import { FastifyReply } from "fastify";
 
-//FIXME: use the function to handle prisma errors
-export function handlePrismaError(err: unknown, reply: FastifyReply) {
-    // Handling Prisma errors
-    if (err instanceof PrismaClientKnownRequestError) {
-        if (err.code === "P2002") {
-            return reply.status(409).send({
-                error: "Email already exists",
-            });
-        }
-    }
+// //FIXME: use the function to handle prisma errors
+// export function handlePrismaError(err: unknown, reply: FastifyReply) {
+//     // Handling Prisma errors
+//     if (err instanceof PrismaClientKnownRequestError) {
+//         if (err.code === "P2002") {
+//             return reply.status(409).send({
+//                 error: "Email already exists",
+//             });
+//         }
+//     }
 
-    console.error(err);
+//     console.error(err);
 
-    return reply.status(500).send({
-        error: "Internal Server Error",
-    });
-}
+//     return reply.status(500).send({
+//         error: "Internal Server Error",
+//     });
+// }
